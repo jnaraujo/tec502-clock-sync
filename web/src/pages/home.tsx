a919b40fde537f1d73df2e8c75922168b4922db7
Jônatas A. <jonatasaraujo2257@gmail.com>
2024-07-20 20:27:28 -0300

    chore: Update postcss.config.js, tsconfig.json, tsconfig.app.json, .prettierrc.cjs, components.json, vite.config.ts, .eslintrc.cjs, and remove App.css and App.tsx

---
+++ web/src/pages/home.tsx
@@ -0,0 +1,7 @@
+export function HomePage() {
+  return (
+    <main className="bg-muted flex min-h-[100svh] flex-col justify-between gap-6 overflow-auto p-6 font-sans">
+      <h1>WoW</h1>
+    </main>
+  )
+}
