06658d449d47b61ea50db1e3a0dc38e5cc9ba44d
Jônatas A. <jonatasaraujo2257@gmail.com>
2024-07-24 20:08:00 -0300

    feat: update heading color to zinc-900 on home page

--- web/src/pages/home.tsx
+++ web/src/pages/home.tsx
@@ -7,7 +7,7 @@ export function HomePage() {
   return (
     <main className="flex min-h-[100svh] flex-col items-center justify-center overflow-auto bg-pink-300 font-sans">
       <article className="max-w-screen-lg space-y-2 rounded-lg bg-background p-6">
-        <h1 className="text-xl font-semibold text-pink-500">Relógios:</h1>
+        <h1 className="text-xl font-semibold text-zinc-900">Relógios:</h1>
         <div className="grid grid-cols-2 grid-rows-2 gap-x-14 gap-y-6">
           {clocks?.map((clock) => {
             return <Clock key={clock.self_id} clock={clock} />
