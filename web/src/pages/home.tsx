1b3f12437fa399ad657f8b35a1df557942ffc818
Jônatas A. <jonatasaraujo2257@gmail.com>
2024-07-24 19:18:08 -0300

    chore: adjust gap-y value for clock grid on home page

--- web/src/pages/home.tsx
+++ web/src/pages/home.tsx
@@ -8,7 +8,7 @@ export function HomePage() {
     <main className="flex min-h-[100svh] flex-col items-center justify-center overflow-auto bg-muted font-sans">
       <article className="max-w-screen-lg space-y-2 rounded-lg bg-background p-6">
         <h1 className="text-xl font-semibold text-zinc-900">Relógios:</h1>
-        <div className="grid grid-cols-2 grid-rows-2 gap-x-14 gap-y-4">
+        <div className="grid grid-cols-2 grid-rows-2 gap-x-14 gap-y-6">
           {clocks?.map((clock) => {
             return <Clock key={clock.self_id} clock={clock} />
           })}
